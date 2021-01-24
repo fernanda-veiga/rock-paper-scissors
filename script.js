66526c178afe42298aea5a6dad19bf3fcbd4a8c5
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-24 20:24:54 -0300

    Added a function to change the score of the players in the html

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-const rock = document.getElementById("rock-btn")
-const paper = document.getElementById("paper-btn")
-const scissors = document.getElementById("scissors-btn")
+const rock = document.getElementById("rock-btn");
+const paper = document.getElementById("paper-btn");
+const scissors = document.getElementById("scissors-btn");
+const computerScoreText = document.getElementById("computer-score");
+const playerScoreText = document.getElementById("player-score");
 
-//EVENT LISTENER
+let computerPoints = 0;
+let playerPoints = 0;
+let result
 
-rock.addEventListener("click", computerPlay);
-paper.addEventListener("click", computerPlay);
-scissors.addEventListener("click", computerPlay);
+//EVENT LISTENERS
+
+rock.addEventListener("click", function(){
+    game("rock")
+});
+paper.addEventListener("click", function(){
+    game("paper")
+});
+scissors.addEventListener("click", function(){
+    game("scissors")
+});
+
+console.log(computerScore);
+console.log(playerScore);
+
+function game(playerSelection) {
+    result = playRound(playerSelection, computerPlay());
+
+    if (result == "You won") {
+        playerScore = playerScore + 1;
+    } 
+    else if (result == "You lost") {
+        computerScore = computerScore + 1;
+    } 
+    else {
+        playerScore = playerScore + 1;
+        computerScore = computerScore + 1;
+    }
+}
+
+function changeScore(score, player) {
+    score = score + 1;
+    if (player == "computer") {
+        computerScoreText.innerHTML = "SCORE: " + score;
+    }
+    else (player == "player") {
+        playerScoreText.innerHTML = "SCORE: " + score;
+    }
+    return;
+}
 
 //FUNCTIONS
 
@@ -18,6 +59,7 @@ function computerPlay() {
     return computerSelection;
 }
 
+//One round of the game
 function playRound(playerSelection, computerSelection) {
 
     let gameDatabase = {
@@ -27,42 +69,16 @@ function playRound(playerSelection, computerSelection) {
     };
 
     if (gameDatabase[playerSelection].won == computerSelection) {
+        console.log("win");
         return "You won"; 
     }
     else if (gameDatabase[playerSelection].lose == computerSelection) {
+        console.log("lost");
         return "You lost"; 
     }
     else {
+        console.log("tie");
         return "It's a tie";
     }
 }
 
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-
-    for (let i=0; i<5; i++) {
-
-
-        let result = playRound(playerPlay(), computerPlay());
-
-        if (result == "You won") {
-            playerScore = playerScore + 1;
-        } 
-        else if (result == "You lost") {
-            computerScore = computerScore + 1;
-        } 
-        else {
-            playerScore = playerScore + 1;
-            computerScore = computerScore + 1;
-        }
-    }
-    
-    if (playerScore > computerScore) {
-        return "You won";
-    } else if (playerScore < computerScore) {
-        return "You lost";
-    } else {
-        return "It's a tie";
-    }
-}
