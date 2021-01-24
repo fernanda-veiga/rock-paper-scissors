ba8eb7e64b569dfb12a6b949a7153e29eb0bc58b
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-24 15:24:24 -0300

    Added a prompt user input where the user can choose rock, paper or scissors. Added a function called playRound that represents a round of the game and compares the user input with the computer's choice to determine who won

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Get player's input and make it lower case
+
+let playerSelection = window.prompt("Rock, paper or scissors?");
+playerSelection = playerSelection.toLowerCase();
+
+//FUNCTIONS
+
 function computerPlay() {
     let rockPaperScissors = ["rock", "paper", "scissors"];
     computerSelection = rockPaperScissors[Math.floor(Math.random()*3)];
@@ -5,8 +12,31 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    //return a string to delcare the winner
-    //make the input case insensitive
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return "It's a tie";
+        } else if (computerSelection == "paper") {
+            return "The computer won";
+        } else {
+            return "You won";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You won";
+        } else if (computerSelection == "paper") {
+            return "It's a tie";
+        } else {
+            return "The computer won";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "The computer won";
+        } else if (computerSelection == "paper") {
+            return "You won";
+        } else {
+            return "It's a tie";
+        }
+    } 
 }
 
 function game() {
