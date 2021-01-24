637ceaf0fe47307dc4d6a0027ae04012cc593859
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-24 15:05:50 -0300

    Added a computerPlay function on the js file that chooses between rock, paper and scissors randomly

---
+++ script.js
@@ -0,0 +1,15 @@
+function computerPlay() {
+    let rockPaperScissors = ["rock", "paper", "scissors"];
+    computerSelection = rockPaperScissors[Math.floor(Math.random()*3)];
+    return computerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    //return a string to delcare the winner
+    //make the input case insensitive
+}
+
+function game() {
+    //use playRound inside
+    //play 5 round game and report winner or loser at the end
+}
